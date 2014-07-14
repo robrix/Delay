7a989814a3c1e4749ca6b6b5045f41b1bc2cdcf7
Rob Rix <rob.rix@github.com>
2014-07-14 08:05:17 -0400

    Add delays.

---
+++ Delay/Delay.swift
@@ -0,0 +1,48 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+/// Takes an unevaluated closure \c value and returns a lazily-evaluating wrapper for it.
+func delay<Delayed>(value: @auto_closure () -> Delayed) -> Delay<Delayed> {
+	return Delay(value)
+}
+
+/// A lazily-evaluated value, convertible to its underlying type.
+@final class Delay<Delayed> {
+	/// The underlying value. Lazily evaluates and produces the receiving type.
+	@lazy var value: Delayed = {
+		let value = self._thunk!()
+		self._thunk = nil
+		return value
+	}()
+	
+	init(_ thunk: () -> Delayed) {
+		_thunk = thunk
+	}
+	
+	/// Conversion to the underlying type.
+	@conversion func __conversion() -> Delayed {
+		return value
+	}
+	
+	var _thunk: (() -> Delayed)?
+}
+
+
+/// Equality between Delays of Equatable. Forces the Delay.
+func == <Delayed : Equatable> (left: Delay<Delayed>, right: Delay<Delayed>) -> Bool {
+	return ((left as Delayed) == (right as Delayed))
+}
+
+/// Equality between Delays of Equatable and underlying type. Forces the Delay.
+func == <Delayed : Equatable> (left: Delay<Delayed>, right: Delayed) -> Bool {
+	return (left as Delayed) == right
+}
+
+func == <Delayed : Equatable> (left: Delayed, right: Delay<Delayed>) -> Bool {
+	return left == (right as Delayed)
+}
+
+
+/// Hashing for Delays of Hashable. Forces the Delay.
+func hashValue<Delayed : Hashable>(delay: Delay<Delayed>) -> Int {
+	return (delay as Delayed).hashValue
+}
