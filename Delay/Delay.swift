83a44d6e12d23d113d1b9cb786398593f385e238
Adam Sharp <adsharp@me.com>
2014-08-01 21:47:11 +1000

    Add access control

--- Delay/Delay.swift
+++ Delay/Delay.swift
@@ -1,25 +1,25 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
 /// Takes an unevaluated closure \c value and returns a lazily-evaluating wrapper for it.
-func delay<Delayed>(value: @auto_closure () -> Delayed) -> Delay<Delayed> {
+public func delay<Delayed>(value: @auto_closure () -> Delayed) -> Delay<Delayed> {
 	return Delay(value)
 }
 
 /// A lazily-evaluated value, convertible to its underlying type.
-final class Delay<Delayed> {
+public final class Delay<Delayed: Reflectable> {
 	/// The underlying value. Lazily evaluates and produces the receiving type.
-	lazy var value: Delayed = {
+	public lazy var value: Delayed = {
 		let value = self._thunk!()
 		self._thunk = nil
 		return value
 	}()
 	
-	init(_ thunk: () -> Delayed) {
+	public init(_ thunk: () -> Delayed) {
 		_thunk = thunk
 	}
 	
 	/// Conversion to the underlying type.
-	func __conversion() -> Delayed {
+	public func __conversion() -> Delayed {
 		return value
 	}
 	
@@ -43,6 +43,6 @@ func == <Delayed : Equatable> (left: Delayed, right: Delay<Delayed>) -> Bool {
 
 
 /// Hashing for Delays of Hashable. Forces the Delay.
-func hashValue<Delayed : Hashable>(delay: Delay<Delayed>) -> Int {
+public func hashValue<Delayed : Hashable>(delay: Delay<Delayed>) -> Int {
 	return (delay as Delayed).hashValue
 }
